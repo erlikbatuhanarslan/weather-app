f00abeddb4501de29f2b36604e2eeae14a62ac4d
erlikbatuhanarslan <erlikbatuhanarslan@gmail.com>
2023-08-19 21:54:55 +0300

    weather app react

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+    MDBFooter,
+} from 'mdb-react-ui-kit';
+
+export default function App() {
+    return (
+        <MDBFooter className='footer mt-5'>
+            <hr />
+            <div className='text-center p-3'>
+                © 2023 Copyright:
+                <a href='https://www.linkedin.com/in/batuhan-arslan-42960a267/'>
+                    Erlik Batuhan Arslan
+                </a>
+            </div>
+        </MDBFooter>
+    );
+}
